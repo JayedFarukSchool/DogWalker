b5e49027f00dd06617abaf61d1a2bab1096b4add
JayedFaruk <jayedf@nycstudents.net>
2025-09-19 15:22:59 -0400

    2/10 pages

---
+++ src/DogWalkCompany.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DogWalkCompany {
+    private int[] schedule;
+    public DogWalkCompany(){
+        schedule = new int[24];
+    }
+
+    public String toString(){
+        String s = "";
+        for (int i=0; i < schedule.length; i++)
+            s+= i + "" + schedule[i] + "\n";
+        return s;
+    }
+
+    public int addDogs(){
+        Scanner s = new Scanner (System.in);
+
+    }
+}
